72cab3deea0756497c347eef8d5df0a08a6b5bde
nakajima <na_masabon@yahoo.co.jp>
2014-07-01 14:21:19 +0900

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,20 @@
+include ApplicationHelper
+
+# ヘルパーメソッドとして定義
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+# カスタムマッチャーを使用
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: massage)
+  end
+end
+
+# full_titleテストヘルパー
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
